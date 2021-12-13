a44153462ff183d2e5ce50267dc1a8ab63db93eb
Zayaan Moez <41247476+zayaanmoez@users.noreply.github.com>
2021-12-12 21:07:45 -0500

    Update about.js

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -51,8 +51,8 @@ const About = () => {
     return(
         <div id="about">
             <StyledDiv>
-                <p className="main">Hi, I'm Zayaan. I'm a third year computer science student at Carleton University 
-                    looking for software developer coop jobs for Summer 2022.</p>
+                <p className="main">Hi, I'm Zayaan. I'm a fourth year computer science student at Carleton University 
+                    looking for software engineer coop jobs for Summer 2022.</p>
                 <p className="sub" unselectable="on"> I am a full-stack developer with experience in developing microservices, 
                 server-side applications, data warehousing web apps, and IOS and Android mobile applications. 
                 I like to learn new skills, continue challenging myself, and work on interesting projects in my spare time. 
